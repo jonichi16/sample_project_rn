c6ba15d50ebbd5ed930a38e46af5d40643feff75
jonichi <jonichi16@gmail.com>
2023-11-09 19:55:29 +0800

    Add animation

--- src/common/components/Splash.tsx
+++ src/common/components/Splash.tsx
@@ -1,4 +1,4 @@
-import {Animated, StyleSheet, View} from 'react-native';
+import {Animated, StatusBar, StyleSheet, View} from 'react-native';
 import React, {useEffect, useRef} from 'react';
 
 const Splash = () => {
@@ -11,13 +11,38 @@ const Splash = () => {
         // Animation for eyelid
         Animated.sequence([
           Animated.timing(eyelid, {
-            toValue: 5,
-            duration: 1000,
+            toValue: 8,
+            duration: 400,
             useNativeDriver: true,
           }),
           Animated.timing(eyelid, {
-            toValue: -5,
-            duration: 1000,
+            toValue: -8,
+            duration: 400,
+            useNativeDriver: true,
+          }),
+          Animated.timing(eyelid, {
+            toValue: -8,
+            duration: 400,
+            useNativeDriver: true,
+          }),
+          Animated.timing(eyelid, {
+            toValue: -14,
+            duration: 400,
+            useNativeDriver: true,
+          }),
+          Animated.timing(eyelid, {
+            toValue: 8,
+            duration: 400,
+            useNativeDriver: true,
+          }),
+          Animated.timing(eyelid, {
+            toValue: 20,
+            duration: 400,
+            useNativeDriver: true,
+          }),
+          Animated.timing(eyelid, {
+            toValue: 0,
+            duration: 400,
             useNativeDriver: true,
           }),
         ]),
@@ -25,13 +50,38 @@ const Splash = () => {
         // Animation for eyeball
         Animated.sequence([
           Animated.timing(eyeball, {
-            toValue: {x: 20, y: 80},
-            duration: 1000,
+            toValue: {x: 0, y: 8},
+            duration: 400,
             useNativeDriver: true,
           }),
           Animated.timing(eyeball, {
-            toValue: {x: 100, y: 0},
-            duration: 1000,
+            toValue: {x: 0, y: -8},
+            duration: 400,
+            useNativeDriver: true,
+          }),
+          Animated.timing(eyeball, {
+            toValue: {x: 0, y: -8},
+            duration: 400,
+            useNativeDriver: true,
+          }),
+          Animated.timing(eyeball, {
+            toValue: {x: -14, y: -8},
+            duration: 400,
+            useNativeDriver: true,
+          }),
+          Animated.timing(eyeball, {
+            toValue: {x: -20, y: 8},
+            duration: 400,
+            useNativeDriver: true,
+          }),
+          Animated.timing(eyeball, {
+            toValue: {x: 0, y: 12},
+            duration: 400,
+            useNativeDriver: true,
+          }),
+          Animated.timing(eyeball, {
+            toValue: {x: 0, y: 0},
+            duration: 400,
             useNativeDriver: true,
           }),
         ]),
@@ -44,18 +94,22 @@ const Splash = () => {
   }, []);
 
   return (
-    <View style={[styles.container, styles.background]}>
-      <Animated.View
-        style={[{transform: [{translateY: eyelid}]}, styles.eyelid]}
-      />
-      <Animated.View
-        style={[
-          {transform: eyeball.getTranslateTransform()},
-          // eyeball.getLayout(),
-          styles.eyeball,
-        ]}
-      />
-    </View>
+    <>
+      <StatusBar backgroundColor={'tomato'} barStyle={'default'} />
+      <View style={[styles.container, styles.background]}>
+        <View style={styles.eye}>
+          <Animated.View
+            style={[{transform: [{translateY: eyelid}]}, styles.eyelid]}
+          />
+          <Animated.View
+            style={[
+              {transform: eyeball.getTranslateTransform()},
+              styles.eyeball,
+            ]}
+          />
+        </View>
+      </View>
+    </>
   );
 };
 
@@ -71,15 +125,33 @@ const styles = StyleSheet.create({
   background: {
     backgroundColor: 'tomato',
   },
-  eyelid: {
+  eye: {
+    backgroundColor: 'white',
     width: 100,
-    height: 20,
-    backgroundColor: 'darkgray',
+    height: 100,
+    borderWidth: 10,
+    borderColor: 'black',
+    borderRadius: 60,
+    overflow: 'hidden',
+  },
+  eyelid: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'darkslategrey',
+    borderBottomWidth: 10,
+    borderColor: 'black',
+    position: 'absolute',
+    left: 0,
+    top: '-50%',
+    zIndex: 10,
   },
   eyeball: {
-    width: 30,
-    height: 30,
-    backgroundColor: 'darkgray',
+    width: 40,
+    height: 40,
+    position: 'absolute',
+    left: 30,
+    top: 22,
+    backgroundColor: 'black',
     borderRadius: 30,
   },
 });
