6a4f4812afc3b078c0abf9320a02ae65897dd526
Jhonel <jonichi16@gmail.com>
2023-11-13 11:58:46 +0800

    Add loading scrren to item

--- src/item/components/index.tsx
+++ src/item/components/index.tsx
@@ -3,21 +3,29 @@ import React, {useEffect} from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../navigations/AppNavigation';
 import useLorem from '../hooks/useLorem';
-import {Spacing} from '../../common/styles';
+import {Colors, Spacing, Typography} from '../../common/styles';
 
 type ItemProps = NativeStackScreenProps<RootStackParamList, 'Item'>;
 
 const Item = ({route, navigation}: ItemProps) => {
   const {item} = route.params;
-  const {lorem} = useLorem();
+  const {lorem, isLoading} = useLorem();
 
   useEffect(() => {
     navigation.setOptions({title: item});
   }, [item, navigation]);
 
+  if (isLoading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <Text style={styles.loading}>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>{lorem}</Text>
+      <Text style={styles.lorem}>{lorem}</Text>
     </View>
   );
 };
@@ -29,5 +37,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     padding: Spacing.spacing.md,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loading: {
+    ...Typography.subHeader.lg,
+    color: Colors.primary.dark,
+  },
+  lorem: {
+    ...Typography.body.md,
+    color: Colors.primary.dark,
   },
 });
--- src/item/hooks/useLorem.ts
+++ src/item/hooks/useLorem.ts
@@ -2,26 +2,31 @@ import {useEffect, useState} from 'react';
 
 const useLorem = () => {
   const [lorem, setLorem] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const fetchLorem = async () => {
-      const response = await fetch('https://loripsum.net/api/1/medium', {
-        mode: 'cors',
-      });
-      const data = await response.text();
-
-      const regex = /<\/?p>/g;
-      setLorem(data.replace(regex, ''));
-    };
-
     try {
+      const fetchLorem = async () => {
+        const response = await fetch('https://loripsum.net/api/1/medium', {
+          mode: 'cors',
+        });
+        const data = await response.text();
+
+        const regex = /<\/?p>/g;
+        setLorem(data.replace(regex, ''));
+      };
+
       fetchLorem();
     } catch (error) {
       console.log(error);
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
     }
   }, []);
 
-  return {lorem};
+  return {lorem, isLoading};
 };
 
 export default useLorem;
